ec500036fc8ff2eff96385cdc891ef5b8ea2f2f4
KarenGelvez <karen.gelvez2@unipamplona.edu.co>
2021-05-17 08:34:11 -0500

    [ADD] pantalla principal

--- src/containers/HomeScreen.tsx
+++ src/containers/HomeScreen.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, FlatList} from 'react-native';
+import {styles} from '../theme/appTheme';
+import {FlatListItem} from '../components/FlatListItem';
+import {FlatListSeparator} from '../components/FlatListSeparator';
+import {menuItems} from '../data/menuItems';
+import {HeaderTitle} from '../components/HeaderTitle';
 
 export const HomeScreen = () => {
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={{...styles.container, ...styles.globalMargin}}>
+      <FlatList
+        ListHeaderComponent={() => <HeaderTitle title="Menu Options" />}
+        data={menuItems}
+        renderItem={({item}) => <FlatListItem menuItem={item} />}
+        keyExtractor={item => item.name}
+        ItemSeparatorComponent={FlatListSeparator}
+      />
     </View>
   );
 };
