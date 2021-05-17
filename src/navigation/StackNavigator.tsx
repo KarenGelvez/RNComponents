3da1f0bd2284bf84e685073c32cc92e8604b3499
KarenGelvez <karen.gelvez2@unipamplona.edu.co>
2021-05-17 08:22:35 -0500

    [ADD] screens

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {HomeScreen} from '../containers/HomeScreen';
+import {Animation01Screen} from '../containers/Animation01Screen';
+import {Animation02Screen} from '../containers/Animation02Screen';
+import {SwitchScreen} from '../containers/SwitchScreen';
+import {AlertScreen} from '../containers/AlertScreen';
+import {TextInputScreen} from '../containers/TextInputScreen';
+import {PullToRefreshScreen} from '../containers/PullToRefreshScreen';
 
 const Stack = createStackNavigator();
 
@@ -9,8 +15,17 @@ export const StackNavigator = () => {
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
+        cardStyle: {
+          backgroundColor: '#fff',
+        },
       }}>
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Animation01" component={Animation01Screen} />
+      <Stack.Screen name="Animation02" component={Animation02Screen} />
+      <Stack.Screen name="Switch" component={SwitchScreen} />
+      <Stack.Screen name="Alert" component={AlertScreen} />
+      <Stack.Screen name="TextInput" component={TextInputScreen} />
+      <Stack.Screen name="PullToRefresh" component={PullToRefreshScreen} />
     </Stack.Navigator>
   );
 };
